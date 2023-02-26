520b5da56150127a69a7134b9e85284770a9376e
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2023-02-26 08:48:07 -0300

    create components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import './styles/global.css';
-import { Habit } from "./componets/Habit";
+import { Header } from './componets/Header';
+import { SummaryTable } from './componets/SummaryTable';
 
-function App() {
+export function App() {
   return (
-    <Habit completed={3}/>
+		<div className="w-screen h-screen flex justify-center items-center">
+			<div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
-
-export default App
